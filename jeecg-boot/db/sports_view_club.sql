bf515dc2eda6f5fa8ab91a6df2572cbdbc9fa203
dskl1 <49867691+dskl1@users.noreply.github.com>
2021-03-04 14:26:51 +0800

    db
    db

--- jeecg-boot/db/sports_view_club.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `sports_view_club`(`id`, `name`, `avatar`) VALUES ('c24e09b6-884d-11ea-8915-dc4a3ef6c20c', '5553的', NULL);
-INSERT INTO `sports_view_club`(`id`, `name`, `avatar`) VALUES ('217edaff-89e8-11ea-bf02-dc4a3ef6c20c', '5553的', NULL);
-INSERT INTO `sports_view_club`(`id`, `name`, `avatar`) VALUES ('c24df1e6-884d-11ea-8915-dc4a3ef6c20c', '在学校', NULL);
